1fa7f3a477f999c9646891d953f16f3727d00ef7
mhesl <mohammadeslami1379@yahoo.com>
2019-12-01 09:59:04 +0330

    recycler view and its model xml created

--- app/src/main/java/com/example/alnumlist/adapter/MainAdapter.java
+++ app/src/main/java/com/example/alnumlist/adapter/MainAdapter.java
@@ -1,12 +1,14 @@
 package com.example.alnumlist.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.alnumlist.R;
 import com.example.alnumlist.models.Album_Model;
 
 import java.util.List;
@@ -23,7 +25,8 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout)
+        return new MyViewHolder(view);
     }
 
     @Override
@@ -33,7 +36,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> 
 
     @Override
     public int getItemCount() {
-        return 0;
+        return album_models.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
