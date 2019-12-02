ee204d0ea7a0d53e2bea0c76a4e45d6ca5a675ff
mhesl <mohammadeslami1379@yahoo.com>
2019-12-02 23:45:28 +0330

    photos adapter and photos fragment completed

--- app/src/main/java/com/example/alnumlist/adapter/PhotosAdapter.java
+++ app/src/main/java/com/example/alnumlist/adapter/PhotosAdapter.java
@@ -1,4 +1,52 @@
 package com.example.alnumlist.adapter;
 
-public class PhotosAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.alnumlist.R;
+import com.example.alnumlist.models.Album_Details_Model;
+
+import java.util.List;
+
+public class PhotosAdapter extends RecyclerView.Adapter<PhotosAdapter.MyViewHolder> {
+    private List<Album_Details_Model> models;
+    private Context context;
+
+    public PhotosAdapter(List<Album_Details_Model> models, Context context) {
+        this.models = models;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.album_detail_model , parent , false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Glide.with(context).load(models.get(position).getUrl()).into(holder.photoModel);
+    }
+
+    @Override
+    public int getItemCount() {
+        return models.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView photoModel;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            photoModel = itemView.findViewById(R.id.photoModel);
+        }
+    }
 }
--- app/src/main/java/com/example/alnumlist/fragments/PhotosFragment.java
+++ app/src/main/java/com/example/alnumlist/fragments/PhotosFragment.java
@@ -8,24 +8,27 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.alnumlist.R;
+import com.example.alnumlist.adapter.PhotosAdapter;
+import com.example.alnumlist.models.Album_Details_Model;
+
+import java.util.List;
 
 public class PhotosFragment extends Fragment {
-    RecyclerView photosRecyclerView;
+    private RecyclerView photosRecyclerView;
+    private PhotosAdapter photosAdapter;
+    private List<Album_Details_Model> models;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.photos_fragment , container , false);
         photosRecyclerView = view.findViewById(R.id.photosRecyclerView);
-
-
-
-
-
-
-
+        photosAdapter = new PhotosAdapter(models , getActivity());
+        photosRecyclerView.setAdapter(photosAdapter);
+        photosRecyclerView.setLayoutManager(new GridLayoutManager(getActivity() , 2));
         return view;
     }
 }
